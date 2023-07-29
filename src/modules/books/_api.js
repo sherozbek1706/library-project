e507f42945362525897194828933f7d4c9e378f3
sherozbekbaxtiyorov <baxtiyorovsherozbek1111@gmail.com>
2023-07-29 15:11:38 +0500

    edit books

--- src/modules/books/_api.js
+++ src/modules/books/_api.js
@@ -1,9 +1,10 @@
-const { post_book, get_book, show_book } = require("./_controller");
+const { post_book, get_book, show_book, edit_book } = require("./_controller");
 
 const router = require("express").Router();
 const isLoggedIn = require("../../shared/auth/isloggedIn");
 router.post("/books", isLoggedIn, post_book);
 router.get("/books", isLoggedIn, get_book);
 router.get("/books/:id", isLoggedIn, show_book);
+router.patch("/books/:id", isLoggedIn, edit_book);
 
 module.exports = router;
--- src/modules/books/_controller.js
+++ src/modules/books/_controller.js
@@ -1,6 +1,7 @@
 const postBook = require("./post-book");
 const getBook = require("./get-book");
 const showBook = require("./show-book");
+const editBook = require("./edit-book");
 
 const post_book = async (req, res) => {
   try {
@@ -15,7 +16,7 @@ const post_book = async (req, res) => {
 const get_book = async (req, res) => {
   try {
     const result = await getBook();
-    
+
     res.json(result);
   } catch (error) {
     res.json(error);
@@ -31,8 +32,20 @@ const show_book = async (req, res) => {
     res.json(error);
   }
 };
+
+const edit_book = async (req, res) => {
+  try {
+    const result = await editBook({ params: req.params.id, body: req.body });
+
+    res.json(result);
+  } catch (error) {
+    res.json(error);
+  }
+};
+
 module.exports = {
   post_book,
   get_book,
   show_book,
+  edit_book,
 };
